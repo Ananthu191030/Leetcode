7dcda2c12c9f2fa76f068386c434e9a7ba337ce3
Ananthapadmanabhan R U <114851869+Ananthu191030@users.noreply.github.com>
2026-03-19 18:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.68%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
+    }
+    int f(int n,int[] dp){
+        if(n==0) return 1;
+        if(n<0)return 0;
+        if(dp[n]!=-1) return dp[n];
+        else{
+        int take=f(n-1,dp);
+        int nontake=f(n-2,dp);
+        dp[n]=take+nontake;
+        }
+        return dp[n];
+    }
+}
